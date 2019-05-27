9fde7ff0eda5753c84bfd8471b5d5d7abca28312
zxk175 <zxk175@qq.com>
2019-05-27 16:35:44 +0800

    initial commit

--- well-eureka-server/src/main/java/com/zxk175/well/config/WebSecurityConfig.java
+++ well-eureka-server/src/main/java/com/zxk175/well/config/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package com.zxk175.well.config;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * @author zxk175
@@ -16,9 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 // 关闭跨站请求防护
-                .csrf().disable()
-                // 前后端分离采用JWT 不需要session
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .csrf().disable();
 
         super.configure(httpSecurity);
     }
