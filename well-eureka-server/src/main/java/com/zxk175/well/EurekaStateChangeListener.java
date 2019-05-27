5e116eeb42cf05471f25b37ec5dc75925f9de224
zxk175 <zxk175@qq.com>
2019-05-27 16:15:33 +0800

    initial commit

---
+++ well-eureka-server/src/main/java/com/zxk175/well/EurekaStateChangeListener.java
@@ -0,0 +1,42 @@
+package com.zxk175.well;
+
+import com.netflix.appinfo.InstanceInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.eureka.server.event.*;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zxk175
+ * @since 2019/05/27 16:10
+ */
+@Slf4j
+@Component
+public class EurekaStateChangeListener {
+
+    @EventListener
+    public void listen(EurekaInstanceRegisteredEvent event) {
+        InstanceInfo instanceInfo = event.getInstanceInfo();
+        log.info(instanceInfo.getAppName() + "进行注册");
+    }
+
+    @EventListener
+    public void listen(EurekaInstanceCanceledEvent event) {
+        log.info("{}, {} 服务下线", event.getServerId(), event.getAppName());
+    }
+
+    @EventListener
+    public void listen(EurekaInstanceRenewedEvent event) {
+        log.info("{}, {} 服务进行续约", event.getServerId(), event.getAppName());
+    }
+
+    @EventListener
+    public void listen(EurekaRegistryAvailableEvent event) {
+        log.info("注册中心 启动");
+    }
+
+    @EventListener
+    public void listen(EurekaServerStartedEvent event) {
+        log.info("Eureka Server 启动");
+    }
+}
