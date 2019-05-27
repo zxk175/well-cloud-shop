94cae1b2a722e8b0547ea74746ee86b483718639
zxk175 <zxk175@qq.com>
2019-05-27 15:19:24 +0800

    initial commit

--- well-eureka-server/src/main/java/com/zxk175/well/config/WebSecurityConfig.java
+++ well-eureka-server/src/main/java/com/zxk175/well/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.zxk175.well.config;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * @author zxk175
@@ -11,20 +12,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    /**
-     * 2.1版本的security默认加上了 csrf 拦截, 所以需要通过重写方法, 把csrf拦截禁用
-     * https://github.com/yinjihuan/spring-cloud/issues/1
-     *
-     * <pre>
-     *     This is because @EnableWebSecurity is now added by default when Spring Security is on the classpath.
-     *     This enable CSRF protection by default. You will have the same problem in 1.5.10 if you add @EnableWebSecurity.
-     *     One work around, which is not the most secure workaround if you have browsers using the Eureka dashboard, is to disable CSRF protection.
-     *     This can be done by adding the following configurations to your app.
-     * </pre>
-     */
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable();
+        httpSecurity
+                // 关闭跨站请求防护
+                .csrf().disable()
+                // 前后端分离采用JWT 不需要session
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         super.configure(httpSecurity);
     }
