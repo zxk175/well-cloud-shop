34a218641dd55691ba71381ae98da57ab0450e44
zxk175 <zxk175@qq.com>
2019-05-27 17:51:16 +0800

    initial commit

--- well-boot-admin-server/src/main/java/com/zxk175/well/config/WebSecurityConfig.java
+++ well-boot-admin-server/src/main/java/com/zxk175/well/config/WebSecurityConfig.java
@@ -1,11 +1,8 @@
 package com.zxk175.well.config;
 
-import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * @author zxk175
@@ -14,36 +11,11 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final String adminContextPath;
-
-
-    public WebSecurityConfig(AdminServerProperties adminServerProperties) {
-        this.adminContextPath = adminServerProperties.getContextPath();
-    }
-
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-        successHandler.setTargetUrlParameter("redirectTo");
-
-        httpSecurity.authorizeRequests()
-                .antMatchers(adminContextPath + "/assets/**").permitAll()
-                .antMatchers(adminContextPath + "/login").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler)
-                .and()
-                .logout().logoutUrl(adminContextPath + "/logout")
-                .and()
-                .httpBasic()
-                .and()
-                .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .ignoringAntMatchers(
-                        adminContextPath + "/instances",
-                        adminContextPath + "/actuator/**"
-                )
-                .disable();
+        httpSecurity
+                // 关闭跨站请求防护
+                .csrf().disable();
 
         super.configure(httpSecurity);
     }
