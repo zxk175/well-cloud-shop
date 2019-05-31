9b40b7b77dd693966cbcb9d3dbec5f44799c5676
zxk175 <zxk175@qq.com>
2019-05-31 17:31:38 +0800

    initial commit

--- well-zuul-server/src/main/java/com/zxk175/well/WellZuulServerApplication.java
+++ well-zuul-server/src/main/java/com/zxk175/well/WellZuulServerApplication.java
@@ -2,6 +2,7 @@ package com.zxk175.well;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @EnableZuulProxy
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WellZuulServerApplication {
 
     public static void main(String[] args) {
