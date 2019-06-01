d28a36a4d261afd68be798b2e82a6583400eaf6c
zxk175 <zxk175@qq.com>
2019-06-01 23:16:18 +0800

    edit null login

--- well-common/well-common-core/src/main/java/com/zxk175/well/config/filter/RequestAuthFilter.java
+++ well-common/well-common-core/src/main/java/com/zxk175/well/config/filter/RequestAuthFilter.java
@@ -3,6 +3,7 @@ package com.zxk175.well.config.filter;
 import cn.hutool.core.util.ArrayUtil;
 import com.zxk175.well.common.consts.Const;
 import com.zxk175.well.common.util.FilterUtil;
+import com.zxk175.well.common.util.MyStrUtil;
 import com.zxk175.well.common.util.common.CommonUtil;
 import com.zxk175.well.common.util.jwt.JwtAuthUtil;
 
@@ -43,7 +44,7 @@ public class RequestAuthFilter implements Filter {
                 filterChain.doFilter(request, response);
             } else {
                 String myHost = httpRequest.getHeader(Const.MY_HOST);
-                String fullUrl = myHost + CommonUtil.getLoginUrl(httpRequest);
+                String fullUrl = (MyStrUtil.isBlank(myHost) ? "" : myHost) + CommonUtil.getLoginUrl(httpRequest);
                 HttpServletResponse httpResponse = (HttpServletResponse) response;
                 httpResponse.sendRedirect(fullUrl);
             }
