035785595f1f2730f27b1cf03e32765291f2e8f5
zxk175 <zxk175@qq.com>
2019-08-09 17:27:36 +0800

    edit code

--- well-base/well-gateway/src/main/java/com/zxk175/well/filter/log/RouteRequestLogFilter.java
+++ well-base/well-gateway/src/main/java/com/zxk175/well/filter/log/RouteRequestLogFilter.java
@@ -39,15 +39,6 @@ public class RouteRequestLogFilter implements GlobalFilter, Ordered {
         // 记录代理请求
         GatewayLogUtil.recorderRouteRequest(exchange);
 
-        String path = originRequest.getURI().getPath();
-        if (MyStrUtil.neEndWithIgnoreCase(path, MySwaggerProvider.API_URI)) {
-            return filterChain.filter(exchange);
-        }
-
-        String headerName = "X-Forwarded-Prefix";
-        String basePath = path.substring(0, path.lastIndexOf(MySwaggerProvider.API_URI));
-        ServerHttpRequest newRequest = originRequest.mutate().header(headerName, basePath).build();
-
-        return filterChain.filter(exchange.mutate().request(newRequest).build());
+        return filterChain.filter(exchange);
     }
 }
