d14e294088eda82b7f3c252eb74709ec64444b97
zxk175 <zxk175@qq.com>
2019-09-02 09:24:16 +0800

    initial commit

---
+++ well-provider/well-provider-gateway/src/main/java/com/zxk175/well/gateway/WellProviderGatewayApplication.java
@@ -0,0 +1,27 @@
+package com.zxk175.well.gateway;
+
+import com.zxk175.well.base.consts.Const;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author zxk175
+ * @since 2019-08-28 11:09
+ */
+@Controller
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {"com.zxk175.well.gateway", Const.SCAN_CORE})
+public class WellProviderGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WellProviderGatewayApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:/swagger-ui.html";
+    }
+}
